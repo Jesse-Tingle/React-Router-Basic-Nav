7928d39f2bf7923571bc0b925fad043361365a10
Jesse-Tingle <jtingle0@gmail.com>
2019-09-05 00:08:37 -0400

    add BrowswerRouter to index.js and import Route from react-router-dom

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root')
+);
